b9ba64b1ec28975c0162874b4e4db6d91ab9b571
Dmitriy <ambrose163@mail.ru>
2023-08-03 19:04:12 +0400

    save

--- HW_5/task2.py
+++ HW_5/task2.py
@@ -1,13 +1,18 @@
 """
-Напишите функцию, принимающую на вход только ключевые параметры и возвращающую словарь,
-где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем,
-используйте его строковое представление.
+Напишите однострочный генератор словаря, который принимает
+на вход три списка одинаковой длины: имена str, ставка int,
+премия str с указанием процентов вида «10.25%». В результате
+получаем словарь с именем в качестве ключа и суммой
+премии в качестве значения. Сумма рассчитывается
+как ставка умноженная на процент премии
 """
 
 
-def function(**kwargs):
-    return {v if v.__hash__ is not None else str(v): k for k, v in kwargs.items()}
+def gen_salary_dict(names_list, salaries_list, bonuses_list):
+    return {name: salary * (1 + float(bonus.strip('%')) / 100) for name, salary, bonus in
+            zip(names_list, salaries_list, bonuses_list)}
 
 
-print(function(a=99, b=100, c=23))
-print(function(a1="Hello world", a2=1, a3="1", a4=[1, 2]))
+names, salaries, bonuses = ["Иван", "Петр", "Василий"], [1000, 5000, 7000], ["10%", "15%", "20%"]
+
+print(gen_salary_dict(names, salaries, bonuses))
